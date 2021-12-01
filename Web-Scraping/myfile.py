1f1f29b8b4454d5f695c479ca63cfd324e51f2eb
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2021-11-30 23:52:05 -0300

    DataFrame

--- Web-Scraping/myfile.py
+++ Web-Scraping/myfile.py
@@ -1,14 +1,21 @@
 import requests
-import re
+import pandas as pd
 from bs4 import BeautifulSoup
 
+def funcprice(price):
+    if price:
+        return (price['content'])
+    else:
+        return (0)
+    
 url='https://sarasotaavionics.com/'
 
 headers = {'User_Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
 
 productlist=[]
+produtolist=[]
 
-r = requests.get('https://sarasotaavionics.com/category/autopilots?s=1000')
+r = requests.get('https://sarasotaavionics.com/category/autopilots?s=300')
 soup = BeautifulSoup(r.content,'lxml')
 
 product = soup.find_all('h4', class_='product-name')
@@ -32,4 +39,16 @@ for ref in productlist:
     cond = auxcond.text.strip().replace("Condition: ","")
     
     price = soup.find('meta', itemprop = 'price')
-    print("$"+price['content'] if price else "$None")
+    auxprice = funcprice(price)
+    
+    produto = {
+        'Name' : name,
+        'Aviability' : avl,
+        'Part_No' : part,
+        'Condition' : cond,
+        'Price' : auxprice
+    }
+    produtolist.append(produto)
+        
+df = pd.DataFrame(produtolist)
+df.to_csv('teste.csv', sep = ';', encoding = 'utf-8', index = False, header = True)
